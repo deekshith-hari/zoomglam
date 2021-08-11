0544080263e2ed9a652eb5e720303360530e74b0
deekshith-hari <deekshith@musubi-management.com>
2021-08-12 02:34:37 +0530

    search function

--- frontend/src/containers/SearchList.jsx
+++ frontend/src/containers/SearchList.jsx
@@ -4,16 +4,21 @@ import { useDispatch, useSelector } from "react-redux";
 import ImgSearch from "../assets/img/icon-search.svg";
 
 import { fetchImages } from "../reducks/images/operations";
-import { getImages } from "../reducks/images/selectors";
+import { getImages, getHasNext } from "../reducks/images/selectors";
 
 import queryString from "query-string";
+import Preview from "../components/Common/Preview";
 
 export default function SearchList() {
   const dispatch = useDispatch();
   const selector = useSelector((state) => state);
   const parsed = queryString.parse(window.location.search);
-  const images = getImages(selector);
   const [search, setSearch] = useState(null);
+  const images = getImages(selector);
+  const hasNext = getHasNext(selector);
+  const [showPreview, setShowPreview] = useState(false);
+  const [selectedImageId, setSelectedImageId] = useState(null);
+
   const [page, setPage] = useState(1);
   console.log(parsed);
 
@@ -27,32 +32,61 @@ export default function SearchList() {
   }, []);
 
   useEffect(() => {
-    dispatch(fetchImages(page, search));
+    if (search) {
+      dispatch(fetchImages(page, search));
+    }
   }, [page, search]);
+
+  const clickShowMore = () => {
+    if (page) {
+      setPage(page + 1);
+    }
+  };
+
+  const clickImage = (imageId) => {
+    setSelectedImageId(imageId);
+    setShowPreview(true);
+  };
 
   return (
     <div>
+      {showPreview && (
+        <Preview
+          setShowPreview={setShowPreview}
+          selectedImageId={selectedImageId}
+        />
+      )}
       <section class="section2">
         <div class="search-box">
           <div class="search-box-inner">
-            <input type="text" placeholder="Office Space" />
-            <img src={ImgSearch} alt="" />
+            <form action="/search" method="get">
+              <input type="text" name="search" placeholder="Office Space" />
+              <img src={ImgSearch} alt="" />
+            </form>
           </div>
         </div>
         <div class="search-result">
-          <p>Search</p>
-          <p></p>
+          <p>Search </p>
+          <p>
+            <b>"{search}"</b>
+          </p>
         </div>
         <ul class="image-list">
           {images.map((image) => (
             <li key={image.id}>
-              <img src={image.image} alt="" />
+              <img
+                src={image.image}
+                alt=""
+                onClick={() => clickImage(image.id)}
+              />
             </li>
           ))}
         </ul>
-        <div class="show-more">
-          <input type="submit" value="Show more" />
-        </div>
+        {hasNext && (
+          <div class="show-more">
+            <input type="submit" value="Show more" onClick={clickShowMore} />
+          </div>
+        )}
       </section>
     </div>
   );
